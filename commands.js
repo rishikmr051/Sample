b12a838e1fff6f0c6a07925f99eab74fc55a1b9d
rishikmr051 <40091516+rishikmr051@users.noreply.github.com>
2018-07-28 10:46:33 +0530

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,4 +1,8 @@
  git rm --cached FileToDiscard.tst
  git cherry-pick <commit id>
  ls -al
+ // to unstage last commit
+ git reset --soft HEAD^ 
 
+ // to delete last committed files
+  git reset --hard HEAD^ 
